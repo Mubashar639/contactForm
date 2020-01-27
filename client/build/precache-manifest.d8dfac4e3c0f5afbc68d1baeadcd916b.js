e9e57c427a594bc15133e39b473e4748793aca5b
Romac <romeldartedar@R-MacBook-Pro.local>
2020-01-27 03:01:53 -0500

    Build included

---
+++ client/build/precache-manifest.d8dfac4e3c0f5afbc68d1baeadcd916b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f03fb93ea6da7e124dc97b76504e834",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dee1ec8f8a6a3913bcfa",
+    "url": "/static/css/2.bd603f24.chunk.css"
+  },
+  {
+    "revision": "e606640f1779f1ef34e3",
+    "url": "/static/css/main.df7b90fe.chunk.css"
+  },
+  {
+    "revision": "dee1ec8f8a6a3913bcfa",
+    "url": "/static/js/2.204eaaf1.chunk.js"
+  },
+  {
+    "revision": "d1c6639dfd9fe04d7dadecb92520cd51",
+    "url": "/static/js/2.204eaaf1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e606640f1779f1ef34e3",
+    "url": "/static/js/main.cdcc4879.chunk.js"
+  },
+  {
+    "revision": "b6825bc21f46fcc5d1fb",
+    "url": "/static/js/runtime-main.1efe2461.js"
+  }
+]);
